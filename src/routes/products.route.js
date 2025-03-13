a6f85f0e4e16b580b771b6d753cfbeaa8ec2264c
fabricio roberts <robertsfabricio68@gmail.com>
2025-03-13 16:40:34 -0300

    dao

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { ProductModel } from "../../models/products.model.js";
+import { ProductosDao } from "../../models/dao/productos.dao.js";
 
 const route = Router();
+const ProductosService = new ProductosDao(ProductModel);
 
 route.get("/", async (req, res) => {
   try {
-    const respuesta = await ProductModel.find();
+    const respuesta = await ProductosService.getAll();
+    if (!respuesta)
+      return res
+        .status(500)
+        .json({ mensaje: "error en la consulta a la base de datos" });
     res.json({
       mensaje: "peticion get llamado correctamente",
       payload: respuesta,
@@ -16,7 +22,11 @@ route.get("/", async (req, res) => {
 });
 route.get("/:pid", async (req, res) => {
   const id = await req.params;
-  const respuesta = await ProductModel.findById(id.pid);
+  const respuesta = await ProductosService.getById(id.pid);
+  if (!respuesta)
+    return res
+      .status(500)
+      .json({ mensaje: "error en la consulta a la base de datos" });
   res.json({
     mensaje: "peticion get by id llamado correctamente",
     payload: respuesta,
@@ -24,24 +34,36 @@ route.get("/:pid", async (req, res) => {
 });
 route.post("/", async (req, res) => {
   const body = req.body;
-  const respuesta = await ProductModel.create(body);
+  const respuesta = await ProductosService(body);
+  if (!respuesta)
+    return res
+      .status(500)
+      .json({ mensaje: "error en la consulta a la base de datos" });
   res.json({ mensaje: "se guardo el producto", payload: respuesta });
 });
 route.put("/:pid", async (req, res) => {
   const body = req.body;
   console.log(body);
   const id = req.params;
-  const result = await ProductModel.findByIdAndUpdate(id.pid, body, {
-    new: true,
-  });
+  const resultado = await ProductosService.update(id.pid, body);
+  if (!respuesta)
+    return res
+      .status(500)
+      .json({ mensaje: "error en la consulta a la base de datos" });
   res
     .status(200)
-    .json({ mensaje: "se actualizo un producto", payload: result });
+    .json({ mensaje: "se actualizo un producto", payload: resultado });
 });
 route.delete("/:pid", async (req, res) => {
   const id = req.params;
-  const result = await ProductModel.deleteOne({ _id: id.pid });
-  res.status(200).json({ mensaje: "se elimino un producto", payload: result });
+  const resultado = await ProductosService.delete(id);
+  if (!respuesta)
+    return res
+      .status(500)
+      .json({ mensaje: "error en la consulta a la base de datos" });
+  res
+    .status(200)
+    .json({ mensaje: "se elimino un producto", payload: resultado });
 });
 
 export default route;
