cc3deec1e720f77bc97d1ce614aa2cc382c8bfe5
fabricio roberts <robertsfabricio68@gmail.com>
2025-03-14 14:36:30 -0300

    conexion-carro-producto-avance

---
+++ models/dao/carritos.dao.js
@@ -0,0 +1,16 @@
+import { ProductModel } from "../products.model.js";
+import { Common } from "./common.dao.js";
+import { ProductosDao } from "./productos.dao.js";
+const InternDaoProductService = new ProductosDao(ProductModel);
+export class CarritosDao extends Common {
+  async AddProductToCart(ID, body) {
+    const productlist = await InternDaoProductService.getAll();
+    console.log(productlist);
+    const product = await InternDaoProductService.getById(ID);
+    if (product) {
+      console.log(product.title);
+    } else {
+      console.log("no existe");
+    }
+  }
+}
--- src/routes/carts.route.js
+++ src/routes/carts.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { CartsModel } from "../../models/carts.model.js";
 import { ProductModel } from "../../models/products.model.js";
+import { CarritosDao } from "../../models/dao/carritos.dao.js";
 
 const route = Router();
+const CarritoService = new CarritosDao(CartsModel);
 
 route.get("/", async (req, res) => {
   try {
-    const respuesta = await CartsModel.find();
+    const body = req.body;
+    const respuesta = await CarritoService.getAll();
+    CarritoService.AddProductToCart("67d2f6e31d213d1fd92e781f", body);
     res.json({
       mensaje: "peticion get llamado correctamente",
       payload: respuesta,
@@ -17,7 +21,7 @@ route.get("/", async (req, res) => {
 });
 route.post("/", async (req, res) => {
   const body = req.body;
-  const respuesta = await CartsModel.create(body);
+  const respuesta = await CarritoService.create(body);
   res.json({ mensaje: "se guardo el producto", payload: respuesta });
 });
 
